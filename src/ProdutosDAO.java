aaa3a949bf8022e79de9c446662ea4b2ae3118d2
igorfjf <igorfjf2010@gmail.com>
2025-03-18 19:46:44 -0300

    Método listarProdutos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,7 +7,6 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -15,38 +14,51 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.sql.SQLException;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public int cadastrarProduto (ProdutosDTO produto){
-         conn = new conectaDAO().connectDB();
-        
-         int status;
-         try {
-             prep = conn.prepareStatement("INSERT INTO produtos(nome, valor) VALUES(?,?)");
-             prep.setString(1, produto.getNome());
-             prep.setInt(2, produto.getValor());
- 
-             status = prep.executeUpdate();
-             return status;
-         } catch (SQLException ex) {
-             System.out.println("Erro ao cadastrar: " + ex.getMessage());
-             return ex.getErrorCode();
-         }
-     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
-    }
-    
-    
-    
-        
-}
 
+    public int cadastrarProduto(ProdutosDTO produto) {
+        conn = new conectaDAO().connectDB();
+
+        int status;
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos(nome, valor) VALUES(?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+
+            status = prep.executeUpdate();
+            return status;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex.getMessage());
+            return ex.getErrorCode();
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+        String sql = "SELECT * FROM produtos WHERE nome LIKE ?";
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produtos = new ProdutosDTO();
+
+                produtos.setId(rs.getInt("id"));
+                produtos.setNome(rs.getString("nome"));
+                produtos.setValor(rs.getInt("valor"));
+                produtos.setStatus(rs.getString("status"));
+
+                listagem.add(produtos);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
