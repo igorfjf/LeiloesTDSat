4b2a4ca18c84cb102e7c99886bb1c091ae2c2987
igorfjf <igorfjf2010@gmail.com>
2025-03-18 20:05:57 -0300

    Método listarProdutos funcionando

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +8,9 @@
  *
  * @author Adm
  */
+=======
+
+>>>>>>> parent of ffa49c2 (Método cadastrarProduto funcionando)
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -20,6 +24,53 @@ public class ProdutosDAO {
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+<<<<<<< HEAD
+=======
+    
+    public int cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
+       
+        int status;
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos(nome, valor) VALUES(?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+
+            status = prep.executeUpdate();
+            return status;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao cadastrar: " + ex.getMessage());
+            return ex.getErrorCode();
+        }
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        String sql = "SELECT * FROM produtos WHERE nome LIKE ?";
+        ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+        
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                ProdutosDTO produtos = new ProdutosDTO();
+
+                produtos.setId(rs.getInt("id"));
+                produtos.setNome(rs.getString("nome"));
+                produtos.setValor(rs.getInt("valor"));
+                produtos.setStatus(rs.getString("status"));
+
+                listagem.add(produtos);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listagem;
+    }
+}
+>>>>>>> parent of ffa49c2 (Método cadastrarProduto funcionando)
 
     public int cadastrarProduto(ProdutosDTO produto) {
         conn = new conectaDAO().connectDB();
